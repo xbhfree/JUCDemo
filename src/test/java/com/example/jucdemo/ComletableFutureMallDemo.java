ee92c82569d836d1bf82bbd587c337327e094ff4
xbhfree <xbhfree@163.com>
2023-01-22 09:22:03 +0800

    CompletableFuture 异步请求比价

--- src/test/java/com/example/jucdemo/ComletableFutureMallDemo.java
+++ src/test/java/com/example/jucdemo/ComletableFutureMallDemo.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
@@ -22,13 +23,36 @@ public class ComletableFutureMallDemo {
                         String.format(productName + " in %s price is %.2f", netMall.getNetMallName(), netMall.calcPrice(productName)))
                 .collect(Collectors.toList());
     }
+
+    /**
+     * 异步多线程比价
+     * @param list
+     * @param productName
+     * @return
+     */
+    static List<String> getPriceCompletableFuture(List<NetMall> list, String productName){
+        return list
+                .stream()
+                .map(netMall -> CompletableFuture.supplyAsync(() ->
+                        String.format(productName + " in %s price is %.2f", netMall.getNetMallName(), netMall.calcPrice(productName))))
+                .collect(Collectors.toList())
+                .stream()
+                .map(s -> s.join())
+                .collect(Collectors.toList());
+    }
     public static void main(String[] args) {
         Long startTime = System.currentTimeMillis();
         List<String> mysql = getPrice(mallList, "mysql");
         mysql.forEach(System.out::println);
         Long endTime = System.currentTimeMillis();
-        Long time = endTime - startTime;
-        System.out.println("time = " + time);
+        System.out.println("time = " + (endTime - startTime));
+        System.out.println("-----------------------------------------");
+        Long startTime2 = System.currentTimeMillis();
+        List<String> mysql2 = getPriceCompletableFuture(mallList, "mysql");
+        mysql2.forEach(System.out::println);
+        Long endTime2 = System.currentTimeMillis();
+        System.out.println("time = " + (endTime2 - startTime2));
+
     }
 }
 @AllArgsConstructor
