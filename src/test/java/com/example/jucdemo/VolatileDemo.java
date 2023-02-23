1eb8a9d5853c4bab32b5d3d219aa60f5077ce692
xbhfree <xbhfree@163.com>
2023-02-23 22:32:22 +0800

    volatile内存屏障笔记

---
+++ src/test/java/com/example/jucdemo/VolatileDemo.java
@@ -0,0 +1,13 @@
+package com.example.jucdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class VolatileDemo {
+
+    @Test
+    public void test01(){
+
+    }
+}
