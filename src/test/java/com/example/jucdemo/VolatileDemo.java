f24309e73eedb5e6e6273ee0bdab0d733cba6bcb
xbhfree <xbhfree@163.com>
2023-02-25 18:10:15 +0800

    volatile之可见性案例详解

--- src/test/java/com/example/jucdemo/VolatileDemo.java
+++ src/test/java/com/example/jucdemo/VolatileDemo.java
@@ -3,11 +3,31 @@ package com.example.jucdemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.TimeUnit;
+
 @SpringBootTest
 public class VolatileDemo {
 
+    static volatile boolean flag = true;
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 验证volatile的可见性
+     */
     @Test
     public void test01(){
+        new Thread(() -> {
+            System.out.println("开始循环");
+            while (flag){
+
+            }
+            System.out.println("循环结束,flag = " + flag);
+        },"t1").start();
+        try {TimeUnit.SECONDS.sleep(2);} catch (InterruptedException e) {throw new RuntimeException(e);}
+        System.out.println(Thread.currentThread().getName()+ "设置flag为false");
+        flag = false;
 
     }
 }
